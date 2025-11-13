1fa566735e38d4111880bc2cb66c5b741e7135b6
Fyxio <mathieu.sbl@hotmail.com>
2025-11-13 20:03:09 +0100

    Modification call API

--- main.js
+++ main.js
@@ -2,16 +2,25 @@ const appID = `2f5a1799d225797f65692eec5248db12`;
 
 const btnSearch = document.getElementById("search-btn");
 
-btnSearch.addEventListener("click", () =>{
+btnSearch.addEventListener("click", async () => {
     const city = document.getElementById("site-search").value;
-    console.log(city);
-    showMeteo(city);
-})
+
+    const data = await showMeteo(city);
+    const meteoDTO = createWeatherDTO(data);
+
+    console.log(meteoDTO);
+});
 
 async function showMeteo(city) {
-  const repons = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${appID}`);
+  const repons = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${appID}&units=metric&lang=fr`);
   const data = await repons.json();
-  console.log(data);
   return data;
 }
 
+function createWeatherDTO(data){
+  return {
+    city: data.name,
+    temperature: data.main.temp,
+    weather: data.weather[0].main, 
+  };
+}
